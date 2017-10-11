194269214821d87cfe6c1eb3adfe6fba09449308
iykequame <zac6ix@gmail.com>
2017-10-11 22:12:24 +0000

    ...

--- drivers/input/fingerprint/egistec/navi_input.h
+++
@@ -1,6 +0,0 @@
-
-void uinput_egis_init(struct egistec_data *egistec);
-void uinput_egis_destroy(struct egistec_data *egistec);
-void sysfs_egis_init(struct egistec_data *egistec);
-void sysfs_egis_destroy(struct egistec_data *egistec);
-
